a5022f832941dd686c74e790972c5bde02dad118
max232st <max232st@gmail.com>
2022-10-08 21:53:02 +0300

    семинар 2 ДЗ

---
+++ task006_dz/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+int number = new Random().Next(100,1000);
+int symbol1 = number /10 % 10;
+Console.WriteLine(number);
+Console.WriteLine($"{symbol1}");
---
+++ task007_dz/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int number3 = 0;
+ if (number < 100)
+ {
+    Console.WriteLine("третьей цифры нет!!!");
+    return;
+ }
+ else if (number > 100)
+ {
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    number3 = number % 10;
+ }
+ Console.WriteLine ($"Третья цифра = {number3}");
